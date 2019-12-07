01233fb1d0bc263016b30904cc9358eb8e1b410b
MadInArt <knyazvladimir26@gmail.com>
2019-12-07 13:46:14 +0100

    Updates

---
+++ precache-manifest.f807824d8788b23ed3bf3b946096e05a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52f67b80a168740e440ef2c8e9807845",
+    "url": "/calculatorapp/index.html"
+  },
+  {
+    "revision": "94e4cbdaa5c1cc2ba149",
+    "url": "/calculatorapp/static/css/main.ed2610bc.chunk.css"
+  },
+  {
+    "revision": "8b7789b13ad96044c11b",
+    "url": "/calculatorapp/static/js/2.0ff439d7.chunk.js"
+  },
+  {
+    "revision": "a7fc8f0d37ddb08934cc9a90ac7fd961",
+    "url": "/calculatorapp/static/js/2.0ff439d7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "94e4cbdaa5c1cc2ba149",
+    "url": "/calculatorapp/static/js/main.7449afb6.chunk.js"
+  },
+  {
+    "revision": "3fe1aa2dcd01eea9bcd9",
+    "url": "/calculatorapp/static/js/runtime-main.f6641c9b.js"
+  }
+]);
